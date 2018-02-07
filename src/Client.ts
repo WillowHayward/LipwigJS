1703c4f0ff70cb914bbfc754087f5799ed7e0fe4
William Hayward <haywardwilliam1996@gmail.com>
2018-02-07 23:44:20 +1000

    +Host and Client now respond to default message

--- src/Client.ts
+++ src/Client.ts
@@ -4,6 +4,9 @@
 import { SocketUser } from './SocketUser';
 export class Client extends SocketUser {
     constructor(url: string, code: string, data: object = {}) {
-        super();
+        super(url);
+        this.once('joined', (id: string) => {
+            this.id = id;
+        });
     }
 }
--- src/Host.ts
+++ src/Host.ts
@@ -4,6 +4,9 @@
 import { SocketUser } from './SocketUser';
 export class Host extends SocketUser {
     constructor(url: string, options: object = {}) {
-        super();
+        super(url);
+        this.once('created', (id: string) => {
+            this.id = id;
+        });
     }
 }
