85758324d04266a6a437bbc8bbc07dac3265e969
William Hayward <haywardwilliam1996@gmail.com>
2018-02-13 20:45:19 +1000

    +Updated kick tests

--- test/browser/kicking.js
+++ test/browser/kicking.js
@@ -1,25 +1,6 @@
 describe('Kicking', function () {
     it('should kick users', function(done) {
         const host = Lipwig.create('ws://localhost:8080');
-        let user;
-        host.on('created', function(code) {
-            const client = Lipwig.join('ws://localhost:8080', code);
-        });
-
-        host.on('joined', function(connected) {
-            user = connnected;
-            user.kick();
-        });
-
-        host.on('kicked', function(kicked) {
-            if (user === kicked) {
-                done();
-            }
-        });
-    });
-    
-    it('should alert kicked users', function(done) {
-        const host = Lipwig.create('ws://localhost:8080');
         host.on('created', function(code) {
             const client = Lipwig.join('ws://localhost:8080', code);
             client.on('kicked', function() {
