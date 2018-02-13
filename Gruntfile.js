3c98248864a1786e994cd6499dfd6ec222857aa6
William Hayward <haywardwilliam1996@gmail.com>
2018-02-13 12:17:28 +1000

    +Added method to use existing lipwig server for tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,6 +43,20 @@ module.exports = function(grunt) {
             }
         }
     });
+
+    var previous_force_state = grunt.option("force");
+
+    grunt.registerTask("force",function(set){
+        if (set === "on") {
+            grunt.option("force",true);
+        }
+        else if (set === "off") {
+            grunt.option("force",false);
+        }
+        else if (set === "restore") {
+            grunt.option("force",previous_force_state);
+        }
+    });
     
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -52,11 +66,12 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Lipwig server
-    grunt.registerTask('lipwig', function() {
+    grunt.registerTask('lipwigStart', function() {
         const Lipwig = require('lipwig');
         lipwig = new Lipwig();
     });
     // Default task(s).
+    grunt.registerTask('lipwig', ['force:on', 'lipwigStart', 'force:off']);
     grunt.registerTask('build', ['tslint', 'lipwig', 'exec', 'clean:build', 'uglify'])
     grunt.registerTask('default', ['build']);
   };
