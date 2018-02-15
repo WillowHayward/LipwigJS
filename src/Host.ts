ea339bb5b092bf5b8d3d8db787791593962d8a62
William Hayward <haywardwilliam1996@gmail.com>
2018-02-15 17:53:28 +1000

    +Implemented group filtering

--- src/Host.ts
+++ src/Host.ts
@@ -13,6 +13,11 @@ type GroupMap = {
     [index: string]: User[];
 };
 
+type Filter = {
+    whitelist: string[];
+    blacklist: string[];
+};
+
 export class Host extends SocketUser {
     private users: UserMap;
     private groups: GroupMap;
@@ -75,7 +80,38 @@ export class Host extends SocketUser {
 
         this.groups[name] = group.splice(position, 1);
         user.send('unassigned', name);
+    }
 
+    public getGroup(name: string): User[] {
+        const group: User[] = this.groups[name];
+        if (group === undefined) {
+            return [];
+        }
+
+        return group;
+    }
+
+    public send(message: string, filter: Filter, ...args: any[]): void { // tslint:disable-line:no-any
+        let users: User[] = [];
+        if (filter.whitelist === undefined) {
+            filter.whitelist = [];
+        }
+
+        users = this.filter(filter.whitelist, true);
+
+        if (filter.blacklist === undefined) {
+            filter.blacklist = [];
+        }
+
+        const blacklist: User[] = this.filter(filter.blacklist, false);
+
+        const finalArgs: any[] = [message].concat(args); // tslint:disable-line:no-any
+        users.forEach((user: User): void => {
+            if (blacklist.indexOf(user) > -1) {
+                return;
+            }
+            user.send.apply(user, finalArgs);
+        });
     }
 
     protected handle(event: MessageEvent): void {
@@ -122,5 +158,29 @@ export class Host extends SocketUser {
         this.emit('joined', user, message);
 
         return false; // Block second emit
+    }
+
+    private filter(groups: string[], whitelist: boolean): User[] {
+        let filtered: User[] = [];
+
+        if (groups.length === 0 && whitelist) {
+            const users: UserMap = this.getUsers();
+            const userIDs: string[] = Object.keys(users);
+            userIDs.forEach((id: string): void => {
+                filtered.push(users[id]);
+            });
+
+            return filtered;
+        }
+
+        groups.forEach((name: string): void => {
+            filtered = filtered.concat(this.getGroup(name));
+        });
+
+        filtered = filtered.filter((user: User, index: number, users: User[]): boolean => {
+            return users.indexOf(user) === index;
+        });
+
+        return filtered;
     }
 }
